bcae5eb743312fbfb16a3b827d266780180bd750
EvergreenTree97 <tkdfhr7497@naver.com>
2023-11-12 10:40:55 +0900

    [Feat]: Presentation Layer 작성

--- presentation/src/main/java/team/asap/aljo/presentation/MainActivity.kt
+++ presentation/src/main/java/team/asap/aljo/presentation/MainActivity.kt
@@ -3,13 +3,30 @@ package team.asap.aljo.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import team.asap.aljo.domain.login.usecase.LoginUseCase
+import team.asap.aljo.presentation.login.LoginScreen
+import team.asap.aljo.presentation.login.LoginViewModel
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: LoginViewModel by viewModels()
+
+    @Inject
+    lateinit var loginUseCase: LoginUseCase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("hi")
+            LoginScreen(onClickLoginButton = {
+                lifecycleScope.launch {
+                    loginUseCase.kakaoLogin()
+                }
+            })
         }
     }
 }
