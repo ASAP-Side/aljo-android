b434d6c257975ebd4a7c4e1f31a3979c88586176
EvergreenTree97 <tkdfhr7497@naver.com>
2023-11-12 22:53:28 +0900

    [Refactor] 현재 사용하지 않는 Viewmodel 제거

--- domain/src/main/java/team/asap/aljo/domain/login/usecase/LoginUseCase.kt
+++ domain/src/main/java/team/asap/aljo/domain/login/usecase/LoginUseCase.kt
@@ -1,7 +1,6 @@
 package team.asap.aljo.domain.login.usecase
 
 import team.asap.aljo.domain.auth.repsitory.AuthRepository
-import team.asap.aljo.domain.login.model.LoginInfo
 import team.asap.aljo.domain.login.repository.LoginRepository
 import javax.inject.Inject
 
@@ -9,7 +8,10 @@ class LoginUseCase @Inject constructor(
     private val loginRepository: LoginRepository,
     private val authRepository: AuthRepository,
 ) {
-    suspend fun kakaoLogin(): LoginInfo {
-        return loginRepository.kakaoLogin()
+    suspend fun kakaoLogin() {
+        val loginInfo = loginRepository.kakaoLogin()
+        authRepository.signIn(loginInfo = loginInfo).also { user ->
+            //user local 캐싱 등 작업 진행
+        }
     }
 }
--- presentation/src/main/java/team/asap/aljo/presentation/MainActivity.kt
+++ presentation/src/main/java/team/asap/aljo/presentation/MainActivity.kt
@@ -3,18 +3,15 @@ package team.asap.aljo.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import team.asap.aljo.domain.login.usecase.LoginUseCase
 import team.asap.aljo.presentation.login.LoginScreen
-import team.asap.aljo.presentation.login.LoginViewModel
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val viewModel: LoginViewModel by viewModels()
 
     @Inject
     lateinit var loginUseCase: LoginUseCase
@@ -22,11 +19,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LoginScreen(onClickLoginButton = {
-                lifecycleScope.launch {
-                    loginUseCase.kakaoLogin()
-                }
-            })
+            LoginScreen(onClickLoginButton = ::kakaoLogin)
+        }
+    }
+
+    private fun kakaoLogin() {
+        lifecycleScope.launch {
+            loginUseCase.kakaoLogin()
         }
     }
 }
