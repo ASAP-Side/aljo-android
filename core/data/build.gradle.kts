83659ebb950117107e32b1380216a50a5705c757
GunHyung Ham <gdreagon@naver.com>
2024-03-19 22:59:38 +0900

    build : data 모듈의 빌드타입, 네트워크, 테스트 관련 라이브러리 제거

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,46 +1,12 @@
-import org.jetbrains.kotlin.konan.properties.Properties
-
 plugins {
     alias(libs.plugins.aljo.android.library)
     alias(libs.plugins.aljo.junit5)
 }
 
-val properties = Properties().apply {
-    load(rootProject.file("local.properties").inputStream())
-}
-
 android {
     namespace = "team.asap.aljo.data"
-
-    buildTypes {
-        getByName("release") {
-            buildConfigField(
-                "String",
-                "ALJO_BASE_URL",
-                properties.getProperty("ALJO_BASE_URL")
-            )
-        }
-        getByName("debug") {
-            buildConfigField(
-                "String",
-                "ALJO_BASE_URL",
-                properties.getProperty("ALJO_BASE_URL")
-            )
-        }
-    }
 }
 
 dependencies {
-    // module
     implementation(projects.core.domain)
-
-    // library
-    implementation(libs.bundles.retrofit)
-    implementation(platform(libs.okhttp.bom))
-    implementation(libs.okhttp.logging.interceptor)
-    implementation(libs.security)
-    // test
-    testImplementation(libs.mockk)
-    testImplementation(libs.kotlin.coroutines.test)
-    testImplementation(libs.mockk.webserver)
 }
